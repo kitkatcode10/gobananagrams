83b68ee37e7d94087e1a3ab4e5f198a8ee6d8df8
Katherine Wong <katherinewongkw3@gmail.com>
2021-04-03 15:11:16 -0700

    new js updates, tiles add

--- app.js
+++ app.js
@@ -1,18 +1,73 @@
-// <!-- Go Bananas: Bananagrams Variant Game -->
+// Plan: 
+// initiatlize Game
+    // flip the letters, show 10 random letters
+    // load the input field for user to type into 
+
+// Game Logic Structure 
+// wait for a user to click start 
+// Wait for a user to type a word  > Set up event listeners 
+// check that the letters used are in the array of letters shown 
+// let the user know if the word is accepted (if it used the letters; display an alert) 
+    // Accepted: show an accepted message and update the letters available that have not been used / can not make same word 
+    // Not Accepted: show message to try another word 
+    // Not Accepted: less than 2 letters 
+    // Not Accepted: used letter outside of tiles?
+
+// Game Variables
+
+let wins = 0; 
+let losses = 0; 
+let round = 0;
+let wordsMadeCount = 0; 
+let lettersAvailable = 10; // ?? do I need this... 
+let wordsMade = []; // basically need to keep track of the words they made while playing 
+let randomLetters; 
+let words = ["baseball", "birch", "apple", "yellow", "sea"];
+
+// Generate an array of letters for my game tiles 
+
+const tiles = 'abcdefghijklmnopqrstuvwxyz'.split('');
 
 
-// <!-- Psudocode for the overall game play -->
-//  1. Set up, jumbotron for title and header for all pages 
-2. landing page background is flashing/neon-ish image of pink and yellow bananas (see mockup)
-3. Make a clickable start button 
-4. Round 1, generate 10 random letters of the alphabet 
-5. create input box on bottom left for player to type in words 
-6. words are entered by clicking "enter" 
-7. each word entered is collected and shown on in the banana basket (words made)
-8. once all the letters are used or if the player wants more letter they can click more 
-9. if they make the same word, it won't be accepted [if function?]
-10. continue for 3 rounds total 
-11. Wrap up page shows all words made and the count of the words 
+// const wordCount = document.getElementById('words-made-count') 
+
+const wordsMadeCountElement = () => (document.getElementById('words-made-count').innerHTML = wordsMadeCount); 
+// const wordsMadeCountElement =()=>{
+//     console.log("hello")
+//     console.log(wordCount)
+//     wordCount.innerHTML=wordsMadeCount
+//   }
+const wordsMadeElement = () => (document.getElementById('words-made').innerHTML = wordsMade); 
+const displayMsg = (message) => alert(message); 
 
 
-(similar to guess that letter game we built in class) need random letters alphabet - 10 letter generator 
+const tilesContainerElement = (string) => (document.getElementById('letter-tiles-container').innerHTML = string); 
+
+// initialize the game, set up each round... 
+// keep track of how many rounds, the winning condition for each round 
+
+
+
+const generateLetterTilesString = () => {
+    let letterTiles = ''; 
+    tiles.forEach(function (tile) {
+        letterTiles += `<button type="button" class="btn btn-outline-info">${tile}</button>`;
+    }); 
+
+    return letterTiles;
+};
+
+const initGame = () => {
+    wordsMadeCountElement();
+    wordsMadeElement(); 
+    tilesContainerElement(generateLetterTilesString());
+
+};
+
+// Start Game
+
+initGame(); 
+
+
+
+
